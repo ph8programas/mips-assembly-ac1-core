44857964b5c6855d8f22a5ab60059914a9ca5a4c
MatheusPolese <matheuspastorepolese@gmail.com>
2026-06-27 17:57:13 -0300

    Update AT01-g09q2.asm

--- BCC-AC1-AT01-Grupo09/AT01-g09q2.asm
+++ BCC-AC1-AT01-Grupo09/AT01-g09q2.asm
@@ -32,22 +32,21 @@ lw $t1, 4($s0) # t1 recebe B
 slt $t2, $t0, $t1 # if(A<B) t2=1 else t2 =0
 bne $t2, $zero, BMAIOR
 AMAIOR:##MAIOR OU IGUAL
-sw $t0, 8($s0)# guarda em MAIOR a variavel A
+sw $t0, 8($s0)# guarda em A em MAIOR
 j mostrar_resultado
 BMAIOR:
-sw $t1, 8($s0)# guarda em MAIOR a variavel B
+sw $t1, 8($s0)# guarda em B em MAIOR
 j mostrar_resultado
 
 #################################################
 # Mostrar resultado
 #################################################
 mostrar_resultado:
-    # Por aqui as instruções para mostrar 'msg'
-    ori $v0, $zero, 4      # Valor codigo para $v0: 4 (Print String)
+    ori $v0, $zero, 4      # $v0: 4 (Print String)
     addi $a0, $s0, 12      # 0=A 4=B 8=MAIOR 12=MSG
     syscall
 	
-    lw $a0, 8($s0)     # Carrega o valor de MAIOR usando o offset correto 8($s0)
+    lw $a0, 8($s0)     #  8($s0)=maior
     ori $v0, $zero, 1
     syscall
 
